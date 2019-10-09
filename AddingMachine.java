9bf83676924b55883972f27a08f7cf67ae8d2089
sponnap4 <56282913+sponnap4@users.noreply.github.com>
2019-10-09 11:06:08 -0700

    Update AddingMachine.java

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,20 +1,52 @@
+//Sravya Ponnapalli
+//ID: 70642
+//Assignment 2
+//This file contains the code for the methods that
+//add and subtract from the total and concatenate and clear the string. It has
+//two created variables total and history that are used. The values passed through the methods of
+//add and subtract are concatenated into a string and called history. This history can be erased, returned 
+//and also can have values concatenated to it. These values are also added to and subtracted from
+//the current total.
 package cse360assign2;
-public class AddingMachine {
-private int total;
-public AddingMachine () {
-total = 0;  // not needed - included for clarity
+public class AddingMachine 
+{
+	private int total;
+	String history ="0";
+	public AddingMachine ()
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	public int getTotal () //this method returns the current total at hand
+	{
+		return total;
+	}
+	//this method adds the value at hand to the total
+	//it also concatenates the value to the string
+	public void add (int value) 							
+	{
+		total += value;
+		history = history + " " + "+" + " " + value;
+	
+	}
+	//this method adds the value at hand to the total
+	//it also concatenates the value to the string
+	public void subtract (int value) 							
+	{
+		total -= value;
+		history = history + " " + "-" + " " + value;
+	}
+	//this method returns the string at hand that has values concatenated
+	public String toString () 
+	{
+		return history;
+	}
+	//this method gets rid of any content or values in total and history
+	public void clear() 
+	{	
+		total = 0;
+		history = "";
+	}
 }
-public int getTotal () {
-return 0;
-}
-public void add (int value) {
-}
-public void subtract (int value) {
-}
-public String toString () {
-return "";
-}
-public void clear() {
-}
+
 }
 
